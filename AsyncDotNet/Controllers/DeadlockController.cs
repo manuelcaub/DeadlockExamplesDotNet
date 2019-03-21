948d32fedc5e7ae10bd92b20ac0ec0852c70420d
Manuel Cañete <mcanete.plainconcepts@outlook.com>
2019-03-22 00:08:56 +0100

    Async

---
+++ AsyncDotNet/Controllers/DeadlockController.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AsyncDotNet.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DeadlockController : ControllerBase
+    {
+        [HttpGet("no")]
+        public async Task<IActionResult> No()
+        {
+            var context = SynchronizationContext.Current;
+            await Delay();
+            return Ok("NO Deadlock");
+        }
+
+        [HttpGet("itdepends")]
+        public async Task<IActionResult> ItDependsRuntime()
+        {
+            await Delay();
+            Delay().Wait();
+            return Ok("Deadlock?");
+        }
+
+        [HttpGet("configure-await")]
+        public async Task<IActionResult> ConfigureAwait()
+        {
+            var result = await Result().ConfigureAwait(continueOnCapturedContext: false);
+            var blockingResult = Result().Result;
+            return Ok("Deadlock?");
+        }
+
+        private async Task Delay()
+        {
+            await Task.Delay(3000);
+        }
+
+        private async Task<int> Result()
+        {
+            var result = 1;
+            await Task.Delay(3000);
+            return result;
+        }
+    }
+}
---
+++ AsyncDotNetFramework/Controllers/DeadlockController.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace AsyncDotNetFramework.Controllers
+{
+    public class DeadlockController : ApiController
+    {
+        [HttpGet]
+        [Route("deadlock/no")]
+        public async Task<IHttpActionResult> No()
+        {
+            var context = SynchronizationContext.Current;
+            await Delay();
+            return Ok("NO Deadlock");
+        }
+
+        [HttpGet]
+        [Route("deadlock/depends")]
+        public async Task<IHttpActionResult> ItDepends()
+        {
+            await Delay();
+            Delay().Wait();
+            return Ok("Deadlock?");
+        }
+
+        [HttpGet]
+        [Route("deadlock/configure-await")]
+        public async Task<IHttpActionResult> ConfigureAwait()
+        {
+            var result = await Result().ConfigureAwait(continueOnCapturedContext: false);
+            var blockingResult = Result().Result;
+            return Ok("Deadlock?");
+        }
+
+        private async Task Delay()
+        {
+            await Task.Delay(3000);
+        }
+
+        private async Task<int> Result()
+        {
+            var result = 1;
+            await Task.Delay(3000);
+            return result;
+        }
+    }
+}
